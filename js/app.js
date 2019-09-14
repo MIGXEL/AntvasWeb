3737ee42fca85406fc480c10f08fa9a7f93799a3
MIGUEL <miguelantiman.mav@gmail.com>
2019-09-14 16:57:45 -0400

    aplicando animaciones a la pagina

--- js/app.js
+++ js/app.js
@@ -18,37 +18,62 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $(window).scroll(function(){
+    $(window).scroll(function() {
 
         scroll = $(window).scrollTop();
-        if(scroll > 100){
+        if (scroll > 100) {
 
-            $('.menu').css({"position":"fixed"});
-            $('.menu').css({"width":"100%"});
-            $('.menu').css({"top":"0"});
-            $('.menu').css({"background":"rgba(0,0,0,.9)"});
-            $('.menu p').css({"font-size":"40px"});
-            $('.menu').css({"box-shadow":"rgba(0,0,0,.22) 6px 1px 1px"});
-            $('.menu').css({"z-index":"100"});
+            $('.menu').css({ "position": "fixed" });
+            $('.menu').css({ "width": "100%" });
+            $('.menu').css({ "top": "0" });
+            $('.menu').css({ "background": "rgba(0,0,0,.9)" });
+            $('.menu p').css({ "font-size": "40px" });
+            $('.menu').css({ "box-shadow": "rgba(0,0,0,.22) 6px 1px 1px" });
+            $('.menu').css({ "z-index": "100" });
 
-        }else{
+        } else {
 
-            $('.menu').css({"position":"relative"});           
-            $('.menu').css({"background":"none"});
-            $('.menu').css({"box-shadow":"none"});
+            $('.menu').css({ "position": "relative" });
+            $('.menu').css({ "background": "none" });
+            $('.menu').css({ "box-shadow": "none" });
+            $('.menu p').css({ "font-size": "60px" });
+
         }
 
     })
 
-    $('.menuIcon').click(function(){
+    $('.menuIcon').click(function() {
 
         $('header nav').slideToggle();
 
     })
 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
 
-
+    var options = {
+        animateThreshold: 100,
+        scrollPollInterval: 20
+    }
+    $('.aniview').AniView(options);
 
 })
